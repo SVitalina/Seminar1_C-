a5be029c2b1de7cdddecc0ca710f088d0367dea9
Oleksii <zankexd@icloud.com>
2022-06-29 15:45:52 +0200

    Task001

---
+++ Task001/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите 2 числа : ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+ if ( b*b==a)
+ {
+     Console.WriteLine("Квадрат числа " + b + " равен квадрату числа " + a);
+ }
+ else 
+{ 
+    Console.WriteLine("Квадрат числа " + b + " не равен квадрату числа " + a);
+}
