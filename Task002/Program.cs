c87993cb8a44423dfc95bfb54e78b79244955478
Oleksii <zankexd@icloud.com>
2022-06-29 15:46:32 +0200

    Task002

---
+++ Task002/Program.cs
@@ -0,0 +1,32 @@
+﻿int ad = new Random() .Next(1, 7) ;
+Console.WriteLine(ad);
+if (ad == 1)
+
+{
+    Console.WriteLine("Понедельник");
+}
+if (ad == 2)
+{
+    Console.WriteLine("Вторник");
+}
+if (ad == 3)
+{
+    Console.WriteLine("Среда");
+}
+if (ad == 4)
+{
+    Console.WriteLine("Четверг");
+}
+if (ad == 5)
+{
+    Console.WriteLine("Пьятница");
+}
+if (ad == 6)
+{
+    Console.WriteLine("Суббота");
+}
+if (ad == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+
