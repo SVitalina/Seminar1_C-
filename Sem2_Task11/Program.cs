4da24aabedfa8078f8a1a696ae64b23f60df3201
Oleksii <zankexd@icloud.com>
2022-06-30 16:28:59 +0200

    Sam 2 Task11

---
+++ Sem2_Task11/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите програму которая вводит случайное трехзначное 
+// число и удаляет 2 цифру этого числа 
+
+int a = new Random().Next(100, 999);
+int centre = 0; 
+int centre1 = 0;
+int centre2 = 0 ;
+
+centre = a%10;
+centre1 = a/100;
+centre2 = centre1 * 10 + centre;
+
+
+Console.WriteLine(a);
+Console.WriteLine(centre2);
+
