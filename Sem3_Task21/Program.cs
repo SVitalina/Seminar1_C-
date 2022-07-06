fa49c41fda7d748c9cd2acec21b0083428aaf1ef
Oleksii <zankexd@icloud.com>
2022-07-06 17:56:41 +0200

    Sem3 Task 21

---
+++ Sem3_Task21/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 21: Напишите программу, которая принимает 
+//на вход координаты двух точек и находит расстояние между ними 
+//в 2D пространстве.
+
+
+Console.Write("Введите координаты двух точек : ");
+int x1 = Convert.ToInt32 (Console.ReadLine());
+int y1 = Convert.ToInt32 (Console.ReadLine());
+int x2 = Convert.ToInt32 (Console.ReadLine());
+int y2 = Convert.ToInt32 (Console.ReadLine());
+
+double distance = Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2));
+Console.WriteLine(distance);
+
+
+
+
+
