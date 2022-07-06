36a4d6d6b1490a5d39689ef78ba55550419bfe69
Oleksii <zankexd@icloud.com>
2022-07-06 17:55:29 +0200

    Sem3 Task 22

---
+++ Sem3_Task22/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 22: Напишите программу, которая принимает на вход число (N)
+// и выдаёт таблицу квадратов чисел от 1 до N.
+
+int N = new Random().Next(1, 10);
+int N1 = 1;
+int Num = 0;
+
+Console.WriteLine( N );
+
+while (N1<=N)
+{
+    Num = N1*N1;
+    Console.WriteLine(Num);
+    N1++;
+}
+
+
+
+
+
+
