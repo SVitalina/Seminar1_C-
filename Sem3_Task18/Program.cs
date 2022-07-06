0711c0dbc3fafde50fe377233e178395bb3d8554
Oleksii <zankexd@icloud.com>
2022-07-06 17:56:59 +0200

    Sem3 Task 18

---
+++ Sem3_Task18/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 18: Напишите программу, которая
+// по заданному номеру четверти, показывает 
+// диапазон возможных координат точек в этой четверти (x и y).
+
+Console.Write("Введите номер четверти");
+int quarter = Convert.ToInt32 (Console.ReadLine());
+
+if(quarter == 1)
+{
+    Console.WriteLine("0-y+, 0-x-");
+}
+
+if (quarter == 2)
+{
+    Console.WriteLine("y+, x+");
+}
+
+if(quarter == 3)
+{
+    Console.WriteLine("y-, x-");
+}
+
+if(quarter == 4)
+{
+    Console.WriteLine("y-, x+");
+}
