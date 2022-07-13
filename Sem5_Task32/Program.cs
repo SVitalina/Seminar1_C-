cca9ccce624f937ca74558a5a21f71644d28ec27
Oleksii <zankexd@icloud.com>
2022-07-13 14:03:49 +0200

    Sem5 Task 32

---
+++ Sem5_Task32/Program.cs
@@ -0,0 +1,31 @@
+﻿int [] array = new int [5];
+FillArray(array);
+PrintArray(array);
+
+void FillArray(int[]masiv)
+{
+    for (int count = 0; count<masiv.Length; count++)
+    {
+        masiv[count] = new Random().Next(-9, 9);
+    }
+}
+
+
+void PrintArray(int[]masiv)
+{
+    for (int count = 0; count<masiv.Length; count++)
+    {
+       Console.WriteLine (masiv[count]);
+    }
+}
+
+for (int count = 0; count<array.Length; count++)
+    {
+       Console.WriteLine();
+       Console.Write (array[count] * - 1 );
+    }
+
+
+
+
+
